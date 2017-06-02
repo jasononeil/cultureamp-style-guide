c1d5bc148c7c66fa6d3426349d7d1f3f1ef6efff
Kevin Yank <thatguy@kevinyank.com>
2017-06-02 16:28:12 +1000

    Add component demo presets

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Button.scss';
 
-export default function Button() {
-  return <button className={styles.button}>Button</button>;
+export default function Button({ label, disabled }) {
+  return <button disabled={disabled} className={styles.button}>{label}</button>;
 }
--- guide/_atoms/button.md
+++ guide/_atoms/button.md
@@ -1,6 +1,14 @@
 ---
 title: Button
 component: Button
+presets:
+  - name: Basic
+    props:
+      label: Apply
+  - name: Disabled
+    props:
+      label: Apply
+      disabled: true
 ---
 
 A button, for the clickings!
