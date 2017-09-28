dac6094c1e6b90dda0e2f91d6df419e6fada3090
Kevin Yank <thatguy@kevinyank.com>
2017-09-28 11:56:44 +1000

    Convert black to currentColor in icons using SVGO
    We were previously styling the symbols to set fill='black' to fill='currentColor' via CSS, but this change was not inherited by instances of the icons in Firefox, as it was in Safari and Chrome.
    
    This technique of rewriting the color values in the SVGs in the build will be much more reliable.

--- webpack/svgo.config.js
+++ webpack/svgo.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
-    // embedded titles are nonsense; we specify accessibility titles per-icon
+    // embedded titles are nonsense; we specify accessibility titles per icon
     { removeTitle: true },
+    // convert black to currentColor, so this color can be controlled per icon
+    {
+      convertColors: {
+        currentColor: /black|#000|#000000/,
+      },
+    },
   ],
 };
