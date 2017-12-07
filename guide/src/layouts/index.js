8b188ebeb6f19f0df1f5baf9641a63c512ec9169
Jason O'Neil <jason.oneil@gmail.com>
2017-12-07 15:13:00 +1100

    Add CSS reset

--- guide/src/layouts/index.js
+++ guide/src/layouts/index.js
@@ -17,7 +17,7 @@ const pages = [
 const Header = () => (
   <div
     style={{
-      margin: '0 auto',
+      margin: '10px auto',
       maxWidth: 1360,
       padding: '1.45rem 1.0875rem',
     }}
