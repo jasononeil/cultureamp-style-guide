c3d657334aa32ca45e2446760962bbda45822aaa
Jason O'Neil <jason.oneil@gmail.com>
2017-12-13 18:10:11 +1100

    Update the tip card to use icons with the white fillings

--- guide/src/components/tip-card/TipCard.js
+++ guide/src/components/tip-card/TipCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Card from 'components/card';
-import successWhite from 'cultureamp-style-guide/icons/success.svg';
-import exclamation from 'cultureamp-style-guide/icons/exclamation.svg';
+import success from 'cultureamp-style-guide/icons/success-white.svg';
+import exclamation from 'cultureamp-style-guide/icons/exclamation-white.svg';
 import styles from './TextContent.module.scss';
 
 const TipCard = ({ title, type, content }) => (
   <Card
     title={title}
-    icon={type === 'tip' ? successWhite : exclamation}
+    icon={type === 'tip' ? success : exclamation}
     iconColor={type === 'tip' ? 'seedling' : 'coral'}
   >
     <div className={styles.textContent}>
