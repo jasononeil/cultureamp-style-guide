96efb08ffb260ecd7ee7be596ad877ba51728d6d
Jason O'Neil <jason.oneil@gmail.com>
2017-12-14 17:15:28 +1100

    Use gatsby links in markdown content

--- guide/src/components/MarkdownContent.js
+++ guide/src/components/MarkdownContent.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import HtmlContent from './HtmlContent.js';
+import Link from 'gatsby-link';
+
+const LinkRenderer = props => {
+  if (props.href.startsWith('http://') || props.href.startsWith('https://')) {
+    // This is an external link.
+    return <a {...props}>{props.children}</a>;
+  }
+  // This is an internal link, so use Gatsby Link so we get SPA goodness.
+  return <Link to={props.href}>{props.children}</Link>;
+};
 
 const MarkdownContent = ({ content }) => (
   <HtmlContent>
-    <ReactMarkdown source={content} />
+    <ReactMarkdown source={content} renderers={{ link: LinkRenderer }} />
   </HtmlContent>
 );
 
