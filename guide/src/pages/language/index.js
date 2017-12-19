c2ff9bccc2deaf20860fff5f18712368ef13a54d
Jason O'Neil <jason.oneil@gmail.com>
2017-12-19 18:03:47 +0800

    Initial commit of language styleguide
    Note that the images won't load until I start using a markdown-loader for webpack rather than a raw text loader. I've been avoiding this because it means links won't use the GatsbyLink component.
    
    Will need to investigate the best path forward

---
+++ guide/src/pages/language/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import MarkdownContent from 'components/MarkdownContent.js';
+import ProductLanguageStyleGuide from './_ProductLanguageStyleGuide.md';
+
+const Page = () => <MarkdownContent content={ProductLanguageStyleGuide} />;
+
+export default Page;
