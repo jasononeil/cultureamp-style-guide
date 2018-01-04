b8a6c69bc790ada8611805884658ed9adb89d505
Jason O'Neil <jason.oneil@gmail.com>
2018-01-04 15:28:04 +0800

    Initial commit of icons page
    - All the icons listed on the sketch file have an entry, as well as the "white" variants
    - I might need to get rid of the white variants. I've got to be honest, I don't think I understand what's happening here
    - Some of the icons exist in Sketch but are missing as SVGs, I need to create trello cards then remove them.
    - I haven't done the visuals at the top of the page yet.
    - The copy/paste works
    - Full keyboard nav seems to work well

---
+++ guide/src/pages/visuals/icons/_IconTile.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Icon from '../../../../../components/Icon/Icon.js';
+import styles from './IconGrid.module.scss';
+import tick from 'cultureamp-style-guide/icons/check.svg';
+
+class IconTile extends React.Component {
+  state = {
+    recentlyCopied: false,
+  };
+
+  render() {
+    const { title, path, icon } = this.props;
+    return (
+      <button
+        className={classNames({
+          [styles.iconTile]: true,
+          [styles.clicked]: this.state.recentlyCopied,
+        })}
+        onClick={e => this.copyPath()}
+      >
+        <div className={styles.tileWrapper}>
+          <span className={styles.iconLabel}>{title}</span>
+          <span className={styles.iconWrapper}>
+            {icon ? <Icon icon={icon} role="presentational" /> : 'TODO'}
+          </span>
+          {this.renderCopyLabel()}
+        </div>
+      </button>
+    );
+  }
+
+  renderCopyLabel() {
+    const path = this.props.path,
+      copied = this.state.recentlyCopied;
+    return (
+      <span className={styles.copyLabel}>
+        {copied ? 'Copied to Clipboard' : 'Copy Import Path'}
+        {copied && (
+          <span className={styles.tickIcon}>
+            <Icon icon={tick} role="presentational" />
+          </span>
+        )}
+        <input
+          ref={i => (this.input = i)}
+          value={path}
+          readOnly={true}
+          tabIndex={-1}
+          className={styles.pathInput}
+        />
+      </span>
+    );
+  }
+
+  copyPath() {
+    if (this.input) {
+      this.input.select();
+      document.execCommand('copy');
+      this.setState({ recentlyCopied: true });
+      setTimeout(() => this.setState({ recentlyCopied: false }), 2000);
+    }
+  }
+}
+
+IconTile.PropTypes = {
+  title: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  icon: PropTypes.object.isRequired,
+};
+
+export default IconTile;
