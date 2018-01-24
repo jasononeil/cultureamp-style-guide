bfc0edce24d9a0ade1b73010c004882e005e8b5d
Jason O'Neil <jason.oneil@gmail.com>
2018-01-24 11:27:31 +0800

    Use our own loader for adding postcss support.
    This is required because gatsby-plugin-postcss-sass will use css-loader with the setting `importLoaders: 1` (this is non-configurable), and as a result when you use `compose: x from 'y.module.scss'` it will not use both postcss loader and sass loader, and then will start having issues. Painful!
    
    One option that might be fun to explore is using postcss-loader modules rather than css-loader modules, which would give us the option to run NodeJS and output some files when we process. Notably, we could output Elm/FlowType files so we start strictly typing our CSS class names.

--- guide/gatsby-node.js
+++ guide/gatsby-node.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require(`extract-text-webpack-plugin`);
 
 function modifyWebpackConfig(_ref, options) {
   var config = _ref.config,
     stage = _ref.stage;
+  addSassLoaders(config, stage);
   addSrcResolveRoot(config);
   if (stage === 'build-javascript' || stage === 'develop') {
     addSvgLoaders(config);
@@ -11,6 +13,68 @@ function modifyWebpackConfig(_ref, options) {
   addRawLoader(config);
   addMarkdownLoader(config);
   return config;
+}
+
+function addSassLoaders(config, stage) {
+  // This is based on gatsby-plugin-postcss-sass, but matched to our configuration.
+  const sassFiles = /\.s[ac]ss$/,
+    sassModulesFiles = /\.module\.s[ac]ss$/,
+    cssLoader = `css?${JSON.stringify({
+      modules: true,
+      minimize: true,
+      // This ensures `compose:` will use both sass-loader and postcss-loader.
+      importLoaders: 2,
+      localIdentName: `[path]---[name]---[local]---[hash:base64:5]`,
+      sourcMap: stage.startsWith('build'),
+    })}`,
+    sassLoader = `sass`,
+    postCssLoader = `postcss?${JSON.stringify({
+      // Each postcss-loader instance needs a unique ident.
+      ident: 'kaizen-gatsby-website',
+    })}`;
+
+  switch (stage) {
+    case `develop`: {
+      config.loader(`sass`, {
+        test: sassFiles,
+        exclude: sassModulesFiles,
+        loaders: [`style`, `css`, postCssLoader, sassLoader],
+      });
+
+      config.loader(`sassModules`, {
+        test: sassModulesFiles,
+        loaders: [`style`, cssLoader, postCssLoader, sassLoader],
+      });
+      return config;
+    }
+    case `build-css`:
+    case `develop-html`:
+    case `build-javascript`:
+    case `build-html`: {
+      config.loader(`sass`, {
+        test: sassFiles,
+        exclude: sassModulesFiles,
+        loader: ExtractTextPlugin.extract([
+          `css?minimize`,
+          postCssLoader,
+          sassLoader,
+        ]),
+      });
+
+      config.loader(`sassModules`, {
+        test: sassModulesFiles,
+        loader: ExtractTextPlugin.extract(`style`, [
+          cssLoader,
+          postCssLoader,
+          sassLoader,
+        ]),
+      });
+      return config;
+    }
+    default: {
+      return config;
+    }
+  }
 }
 
 function addElmLoader(config) {
