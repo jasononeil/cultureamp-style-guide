eec681689ec0a43c95f74706040fa29e28438c93
Jason O'Neil <jason.oneil@gmail.com>
2018-01-30 07:08:00 +0800

    Moved HeaderBar and MainNav out of Layout module and into their own modules.

--- guide/src/layouts/index.js
+++ guide/src/layouts/index.js
@@ -1,92 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { navigateTo, withPrefix } from 'gatsby-link';
 import classNames from 'classnames';
-import NavigationBar from 'cultureamp-style-guide/components/NavigationBar';
-import Icon from 'cultureamp-style-guide/components/Icon';
-import enso from 'cultureamp-style-guide/icons/ca-monogram.svg';
-import homeIcon from 'cultureamp-style-guide/icons/home.svg';
-import hamburgerIcon from 'cultureamp-style-guide/icons/hamburger.svg';
-import diamond from './diamond.svg';
-import { Link, ExternalLink, ActionLink } from 'components/Link';
 import Breadcrumb from 'components/Breadcrumb';
 import SubNav from 'components/SubNav';
-import sitemap from 'pages/_sitemap.js';
+import MainNav from './mainNav.js';
+import HeaderBar from './headerBar.js';
 import './index.scss';
 import styles from './layout.module.scss';
-
-const Header = ({ toggleNav }) => (
-  <div className={styles.headerBar}>
-    <ActionLink action={() => toggleNav()}>
-      <span className={styles.hamburgerIcon}>
-        <Icon icon={hamburgerIcon} type="img" title="Expand Menu" />
-      </span>
-    </ActionLink>
-    <ExternalLink to="https://github.com/cultureamp/component-library">
-      Download Style Guide
-      <span className={styles.downloadIcon}>
-        <Icon icon={diamond} type="presentation" />
-      </span>
-    </ExternalLink>
-  </div>
-);
-
-const MainNav = ({ openNav }, context) => {
-  const prefix = withPrefix('/'),
-    path = '/' + context.router.route.location.pathname.substr(prefix.length);
-  return (
-    <NavigationBar loading={false} colorScheme="kaizen">
-      {sitemap.children.map(page => (
-        <NavigationBar.Link
-          key={page.title}
-          icon={page.icon}
-          href={page.href}
-          tooltip={page.title}
-          onClick={e => {
-            // Only navigate to this section if we're not already in this section.
-            if (!path.startsWith(page.href)) {
-              navigateTo(page.href);
-            }
-            openNav();
-            e.preventDefault();
-          }}
-          active={path.startsWith(page.href)}
-        />
-      ))}
-      <NavigationBar.Menu
-        tooltip="Culture Amp"
-        header={<div>About Culture Amp</div>}
-        items={[
-          {
-            label: 'Culture Amp',
-            link: 'http://cultureamp.com/',
-          },
-          {
-            label: 'Careers',
-            link: 'https://www.cultureamp.com/about/careers/',
-          },
-          {
-            label: 'Github',
-            link: 'https://github.com/cultureamp/',
-          },
-        ]}
-      >
-        <div className={styles.cultureAmpMenu}>
-          <Icon
-            icon={enso}
-            role="img"
-            title="Culture Amp Logo"
-            inheritSize={true}
-          />
-        </div>
-      </NavigationBar.Menu>
-    </NavigationBar>
-  );
-};
-MainNav.contextTypes = {
-  router: PropTypes.object.isRequired,
-};
 
 class TemplateWrapper extends React.Component {
   state = {
@@ -124,7 +45,7 @@ class TemplateWrapper extends React.Component {
             onClick={() => this.closeNav()}
           />
           <div className={styles.header}>
-            <Header toggleNav={() => this.toggleNav()} />
+            <HeaderBar toggleNav={() => this.toggleNav()} />
           </div>
           <div className={styles.content}>
             <div className={styles.pageContainer}>
